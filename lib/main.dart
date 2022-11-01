18b846e014042d7067dccc587bbb1fb3e1181362
Chhavi Gupta <chhavigupta1705@gmail.com>
2022-11-01 18:20:21 +0530

    Day2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Home_page.dart';
 
 void main() {
   runApp(Skim());
@@ -10,13 +11,7 @@ class Skim extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Material(
-      child:Center(
-      child: Container(
-        child: Text("Welcome to Skiome"),
-      ),
-    )
-      )
+        home: HomePage()
     );
   }
 }
