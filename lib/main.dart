72fd881208851f1eddcf348cc6220e557a8ab3d7
Chhavi Gupta <chhavigupta1705@gmail.com>
2022-11-01 18:24:49 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/Home_page.dart';
+import 'package:flutter_application_1/pages/Home_page.dart';
 
 void main() {
   runApp(Skim());
