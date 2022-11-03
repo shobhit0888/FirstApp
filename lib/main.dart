5df49408680ae179646c4ac35f5c8de6136aea37
Chhavi Gupta <chhavigupta1705@gmail.com>
2022-11-03 08:12:52 +0530

    Day4

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/Home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
 
 void main() {
   runApp(Skim());
@@ -11,7 +12,11 @@ class Skim extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: HomePage()
+        routes: {
+        "/":(context) => LoginPage(),
+        "/home":(context) => HomePage(),
+        "/login":(context) => LoginPage(),
+      },
     );
   }
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart' show BoxFit, BuildContext, Colors, Column, EdgeInsets, ElevatedButton, FontWeight, Image, InputDecoration, Material, Padding, SizedBox, StatelessWidget, Text, TextButton, TextFormField, TextStyle, Widget, debugPrint;
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Welcome to Skiome",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Enter username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: "Enter password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      debugPrint("Welcome to Skiome World");
+                    },
+                    style: TextButton.styleFrom(),
+                    child: Text("Login"),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
