74aeda76b8171d9dd1d2a7ba6042961f1baf3148
Chhavi Gupta <chhavigupta1705@gmail.com>
2022-11-06 19:14:39 +0530

    Day7

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/Home_page.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
 import 'package:flutter_application_1/utils/routes.dart';
+import 'package:flutter_application_1/widgets/themes.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(Skim());
@@ -13,12 +15,16 @@ class Skim extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
+       
       debugShowCheckedModeBanner: false,
       initialRoute: MyRoutes.homeRoute,
-        routes: {
-        "/":(context) => LoginPage(),
-        MyRoutes.homeRoute:(context) => HomePage(),
-        MyRoutes.loginRoute:(context) => LoginPage(),
+      routes: {
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.purple,
+       
         title: Text("Skiome"),
       ),
       body: Center(
